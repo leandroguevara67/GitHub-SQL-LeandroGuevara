4e9bc3c88fcc30cbe098f94330d534d60dd29446
leandroguevara67 <108943012+leandroguevara67@users.noreply.github.com>
2022-07-12 15:08:32 -0300

    Create SQL database.sql

---
+++ SQL database.sql
@@ -0,0 +1,17 @@
+create tale Usuarios(
+id int identity,
+nombre varchar(50),
+apellido varchar(50),
+edad int,
+email varchar(50)
+)
+create prod sp_insertar_usuario(
+@nombre varchar(50),
+@apellido varchar(50),
+@edad int,
+@email varchar(50),
+)
+as
+begin
+insert into Usuarios(nombre,apellido,edad,email) values (@nombre,@apellido,@edad,@email)
+end
